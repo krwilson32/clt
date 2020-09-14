0c7bf275d465cb0c523a25f6567e04643a552182
krwilson32 <70125200+krwilson32@users.noreply.github.com>
2020-09-13 22:51:18 -0400

    add functions reference indigency

---
+++ functions.js
@@ -0,0 +1,7 @@
+Title: Indigency "Flowchart"
+Author: Kisha Wilson
+Before: <link rel="stylesheet" type="text/css" href="https://suffolklitlab.org/howto/qna/style/style.css">
+<div id="icon" style="background-size: 110px 99px;background-image: url('https://suffolklitlab.org/howto/qna/images/maxheadroom.gif');"></div>
+<h2 style="text-align:center;">Indigency "Flowchart"</h2>
+<p><b>This is a <a href="https://www.codingthelaw.org/level/2/#mission_p1">class project</a>. You should not rely on it as a source of legal information!</b></p>
+<p>Answer the following questions to see if you qualify for appointed counsel in Connecticut.</p>
